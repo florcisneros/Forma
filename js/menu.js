66cdf4e8be88e7cbb77cce47db8c888e1216f7a5
unknown <FCisneros@FCisneros.dnd.local>
2022-11-20 18:57:59 -0300

    Forma app, proyecto final

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+function openNavbar() {
+    var element = document.getElementById("navbar");
+    element.classList.add("navbar--open");
+}
+
+function closeNavbar() {
+    var element = document.getElementById("navbar");
+    element.classList.remove("navbar--open");
+}
